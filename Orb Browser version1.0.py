97296d43339ce5a4a8a6218369ac6b824bca08c4
takerin-123 <123164011+takerin-123@users.noreply.github.com>
2024-05-29 17:39:45 +0900

    Update Orb Browser version2.0.py

--- Orb Browser version1.0.py
+++ Orb Browser version1.0.py
@@ -1,18 +1,16 @@
-from PySide6.QtCore import *
-from PySide6.QtWidgets import *
-from PySide6.QtGui import *
-from PySide6.QtWebEngineWidgets import *
-from PySide6.QtPrintSupport import *
-from PySide6.QtWidgets import QLineEdit
-from PySide6.QtWebEngineCore import QWebEngineProfile
-from PySide6.QtCore import Qt, QUrl
-
 import os
 import sys
 import re
 import asyncio
 import aiohttp
-import sqlite3
+import threading
+import xml.etree.ElementTree as ET
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtWebEngineWidgets import *
+from PySide6.QtWebEngineCore import QWebEngineProfile
+import yt_dlp
 
 class AdblockX:
     def __init__(self, page, adBlocker):
@@ -54,254 +52,139 @@ class AdblockX:
         await self.update_lists()
 
     async def updateBlockedContent(self, event):
-        await self.update_lists()         
+        await self.update_lists()
 
-# main window
 class MainWindow(QMainWindow):
-    # constructor
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
-        # creating a vertical bar
         self.vertical_bar = QToolBar("Vertical Bar")
         self.vertical_bar.setOrientation(Qt.Orientation.Vertical)
         self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, self.vertical_bar)
-
-        # creating a tab widget
         self.tabs = QTabWidget()
-
-        # making document mode true
         self.tabs.setDocumentMode(True)
-
-        # adding action when double clicked
         self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
-
-        # adding action when tab is changed
         self.tabs.currentChanged.connect(self.current_tab_changed)
-
-        # making tabs closeable
         self.tabs.setTabsClosable(True)
-
-        # ＋ボタンを追加
         self.add_tab_button = QPushButton("+")
         self.add_tab_button.clicked.connect(self.add_new_tab)
         self.tabs.setCornerWidget(self.add_tab_button, Qt.TopRightCorner)
 
-        # adding action when tab close is requested
+        self.add_tab_button.setStyleSheet("background-color: gray; color: white;")
+        self.add_tab_button.clicked.connect(self.add_new_tab)
+        self.tabs.setCornerWidget(self.add_tab_button, Qt.TopRightCorner)
         self.tabs.tabCloseRequested.connect(self.close_current_tab)
-
-        # making tabs as central widget
         self.setCentralWidget(self.tabs)
-
-        # creating a status bar
         self.status = QStatusBar()
-
-        # setting status bar to the main window
         self.setStatusBar(self.status)
-
-        # creating a tool bar for navigation
         navtb = QToolBar("Navigation")
-
-        # adding tool bar tot he main window
         self.addToolBar(navtb)
-
-        # read "shortcut.db"
         self.load_shortcuts()
-
-        # creating back action
         back_btn = QAction("<", self)
-
-        # setting status tip
         back_btn.setStatusTip("Back to previous page")
-
-        # adding action to back button
-        # making current tab to go back
         back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
-
-        # adding this to the navigation tool bar
         navtb.addAction(back_btn)
-
-        # similarly adding next button
         next_btn = QAction(">", self)
         next_btn.setStatusTip("Forward to next page")
         next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
         navtb.addAction(next_btn)
-
-        # similarly adding reload button
         reload_btn = QAction("○", self)
         reload_btn.setStatusTip("Reload page")
         reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
         navtb.addAction(reload_btn)
-
-        # creating home action
-        home_btn = QAction("ホーム", self)
+        home_btn = QAction("Home", self)
         home_btn.setStatusTip("Go home")
-
-        # creating a tool bar for actions
         self.toolbar = QToolBar("Actions")
         self.addToolBar(Qt.ToolBarArea.TopToolBarArea, self.toolbar)
-
-        # creating a star button
         self.star_button = QAction("☆", self)
         self.star_button.setStatusTip("Add shortcut to vertical bar")
         self.star_button.triggered.connect(self.add_shortcut)
         self.toolbar.addAction(self.star_button)
-
         self.youtube_id_bar = QLineEdit()
-        self.youtube_id_bar.setPlaceholderText("YouTube Video URL")
+        self.youtube_id_bar.setPlaceholderText("YouTube Video ID")
         navtb.addWidget(self.youtube_id_bar)
         self.youtube_id_bar.returnPressed.connect(self.play_youtube_video)
-
-        # adding action to home button
+        self.youtube_download_bar = QLineEdit()
+        self.youtube_download_bar.setPlaceholderText("youtube download")
+        navtb.addWidget(self.youtube_download_bar)
+        self.youtube_download_bar.returnPressed.connect(self.download_youtube_video)
         home_btn.triggered.connect(self.navigate_home)
         navtb.addAction(home_btn)
-
-        # adding a separator
         navtb.addSeparator()
-
-        # creating a line edit widget for URL
         self.urlbar = QLineEdit()
-
-        # adding action to line edit when return key is pressed
         self.urlbar.returnPressed.connect(self.navigate_to_url)
-
-        # adding line edit to tool bar
         navtb.addWidget(self.urlbar)
-
         QWebEngineProfile.defaultProfile().downloadRequested.connect(self.on_downloadRequested)
-
-        # similarly adding stop action
         stop_btn = QAction("X", self)
         stop_btn.setStatusTip("Stop loading current page")
         stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
         navtb.addAction(stop_btn)
-
-        # creating first tab
         self.add_new_tab(QUrl('https://takerin-123.github.io/qqqqq.github.io/'), 'Homepage')
-
         self.vertical_bar = QToolBar("Vertical Bar")
         self.vertical_bar.setOrientation(Qt.Orientation.Vertical)
         self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, self.vertical_bar)
-
         self.status = QStatusBar()
         self.setStatusBar(self.status)
-
-        # showing all the components
         self.show()
-
-        # setting window title
         self.setWindowTitle("")
+        self.setStyleSheet("background-color: black; color: white;")  # 背景色を黒に変更
+        self.tabs.setStyleSheet("QTabBar::tab { color: black; }")
 
-    # method for adding new tab
-    def add_new_tab(self, qurl = None, label ="ブランク"):
-        # if url is blank
+    def add_new_tab(self, qurl=None, label="ブランク"):
         if qurl is None:
-            # creating a google url
             qurl = QUrl('https://takerin-123.github.io/qqqqq.github.io/')
-
-        # creating a QWebEngineView object
         browser = QWebEngineView()
-
-        # setting url to browser
         browser.setUrl(qurl)
-
-        # setting tab index
         i = self.tabs.addTab(browser, label)
         self.tabs.setCurrentIndex(i)
-
-        # adding action to the browser when url is changed
-        # update the url
-        browser.urlChanged.connect(lambda qurl, browser = browser: self.update_urlbar(qurl, browser))
-
-        # adding action to the browser when loading is finished
-        # set the tab title
-        browser.loadFinished.connect(lambda _, i = i, browser = browser: self.tabs.setTabText(i, browser.page().title()))
-
-        # adding action to the browser when icon is changed
-        # set the tab icon
+        browser.urlChanged.connect(lambda qurl, browser=browser: self.update_urlbar(qurl, browser))
+        browser.loadFinished.connect(lambda _, i=i, browser=browser: self.tabs.setTabText(i, browser.page().title()))
         browser.iconChanged.connect(lambda _, i=i, browser=browser: self.tabs.setTabIcon(i, browser.icon()))
 
-    # when double clicked is pressed on tabs
     def tab_open_doubleclick(self, i):
-        # checking index i.e
-        # No tab under the click
         if i == -1:
-            # creating a new tab
             self.add_new_tab()
 
-    # when tab is changed
     def current_tab_changed(self, i):
-        # get the curl
         qurl = self.tabs.currentWidget().url()
-
-        # update the url
         self.update_urlbar(qurl, self.tabs.currentWidget())
-
-        # update the title
         self.update_title(self.tabs.currentWidget())
 
-    # when tab is closed
     def close_current_tab(self, i):
-        # if there is only one tab
         if self.tabs.count() < 2:
-            # do nothing
             return
-
-        # else remove the tab
         self.tabs.removeTab(i)
 
-    # method for updating the title
     def update_title(self, browser):
-        # if signal is not from the current tab
         if browser != self.tabs.currentWidget():
-            # do nothing
             return
-
-        # get the page title
         title = self.tabs.currentWidget().page().title()
-
         formatted_title = title[:7] if len(title) > 7 else title.ljust(7)
         self.setWindowTitle("%s OrbBrowser" % formatted_title)
         self.tabs.setTabText(self.tabs.currentIndex(), formatted_title)
 
-        # set the window title
-        self.setWindowTitle("% s  OrbBrowser" % title)
-
-    # action to go to home
     def navigate_home(self):
-        # go to google
         self.tabs.currentWidget().setUrl(QUrl("https://takerin-123.github.io/qqqqq.github.io/"))
 
-    # method for navigate to url
     def navigate_to_url(self):
         url = self.urlbar.text()
         if "google.com/search?q=" in url:
-            # set the url
             self.tabs.currentWidget().setUrl(QUrl(url))
         else:
             google_search_url = "https://www.google.com/search?q=" + url
-            self.tabs.currentWidgets().setUrl(QUrl(google_search_url))
+            self.tabs.currentWidget().setUrl(QUrl(google_search_url))
 
-    # method to update the url
-    def update_urlbar(self, q, browser = None):
-        # If this signal is not from the current tab, ignore
+    def update_urlbar(self, q, browser=None):
         if browser != self.tabs.currentWidget():
             return
-
-        # set text to the url bar
         self.urlbar.setText(q.toString())
-
-        # set cursor position
         self.urlbar.setCursorPosition(0)
 
-    # YouTubeのURLからビデオIDを抽出する関数 
-    def extract_video_id(self,youtube_url):
-      # YouTubeのURLパターン
-      video_id_pattern = re.compile(r'(?:youtube\.com/watch\?v=|youtu\.be/)([^&?/\s]+)')
-      match = video_id_pattern.search(youtube_url)
-      if match:
-         return match.group(1)
-      return None
+    def extract_video_id(self, youtube_url):
+        video_id_pattern = re.compile(r'(?:youtube\.com/watch\?v=|youtu\.be/)([^&?/\s]+)')
+        match = video_id_pattern.search(youtube_url)
+        if match:
+            return match.group(1)
+        return None
 
     def play_youtube_video(self):
         youtube_url = self.youtube_id_bar.text()
@@ -310,23 +193,22 @@ class MainWindow(QMainWindow):
             embed_url = f"https://www.youtube.com/embed/{video_id}?autoplay=1"
             self.add_new_tab(QUrl(embed_url), 'YouTube Video')
         else:
-            # URLからビデオIDが見つからない場合の処理
-            # 例えばエラーメッセージを表示するなど
             pass
 
     def on_downloadRequested(self, download):
-         download_dir = "C:\\Users\\ユーザー名\\Downloads" 
-         download_filename = download.suggestedFileName()
-         QWebEngineProfile.defaultProfile().setDownloadDirectory(download_dir)
-         download.setDownloadFileName(download_filename)
-         download.accept()
-         self.show_download_progress(download)
- 
+        home_dir = os.path.expanduser("~")
+        download_dir = os.path.join(home_dir, "Downloads")
+        download_filename = download.suggestedFileName()
+        QWebEngineProfile.defaultProfile().setDownloadDirectory(download_dir)
+        download.setDownloadFileName(download_filename)
+        download.accept()
+        self.show_download_progress(download)
+
     def show_download_progress(self, download):
-         progress_bar = QProgressBar(self.status)
-         self.status.addPermanentWidget(progress_bar)
-         download.downloadProgress.connect(lambda bytesReceived, bytesTotal, progress_bar=progress_bar:progress_bar.setValue(int((bytesReceived/bytesTotal) *100) if bytesTotal > 0 else 0))
-         download.finished.connect(lambda progress_bar=progress_bar:progress_bar.deleteLater())
+        progress_bar = QProgressBar(self.status)
+        self.status.addPermanentWidget(progress_bar)
+        download.downloadProgress.connect(lambda bytesReceived, bytesTotal, progress_bar=progress_bar: progress_bar.setValue(int((bytesReceived / bytesTotal) * 100) if bytesTotal > 0 else 0))
+        download.finished.connect(lambda progress_bar=progress_bar: progress_bar.deleteLater())
 
     def update_progress_bar(self, progress_bar, bytesReceived, bytesTotal):
         if bytesTotal > 0:
@@ -337,99 +219,77 @@ class MainWindow(QMainWindow):
         self.status.removeWidget(progress_bar)
         progress_bar.deleteLater()
 
+    def download_youtube_video(self):
+        youtube_url = self.youtube_download_bar.text()
+        video_id = self.extract_video_id(youtube_url)
+        if video_id:
+            threading.Thread(target=self.download_video, args=(video_id,)).start()
+        else:
+            pass
+
+    def download_video(self, video_id):
+        ydl_opts = {'format': 'mp4'}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([f'http://www.youtube.com/watch?v={video_id}'])
+
     def add_shortcut(self):
         current_tab = self.tabs.currentWidget()
         if isinstance(current_tab, QWebEngineView):
             url = current_tab.page().url().toString()
             title = current_tab.page().title()
             shortcut_button = QAction("", self)
-            shortcut_button.setText(current_tab.page().title()) 
+            shortcut_button.setText(current_tab.page().title())
             shortcut_button.setToolTip(url)
             shortcut_button.triggered.connect(lambda: self.tabs.currentWidget().setUrl(QUrl(url)))
             self.vertical_bar.addAction(shortcut_button)
             self.tabs.currentWidget().setUrl(QUrl(url))
-            #  connect "shortcut.db"
-            conn = sqlite3.connect('shortcuts.db')
-            c = conn.cursor()
+            self.save_shortcut_to_xml(title, url)
 
-            # Check if the URL already exists in the database
-            c.execute("SELECT * FROM shortcuts WHERE url = ?", (url,))
-            if c.fetchone() is None:
-              # The URL does not exist, so we can add it to the database
-              c.execute("INSERT INTO shortcuts VALUES (?, ?)", (title, url))
-              conn.commit()
-            else:
-              # The URL already exists, so we should not add it again
-              # Optionally, you can provide user feedback here
-              print("Bookmark already exists.")
+    def save_shortcut_to_xml(self, title, url):
+        if not os.path.exists('shortcuts.xml'):
+            root = ET.Element("shortcuts")
+            tree = ET.ElementTree(root)
+            tree.write('shortcuts.xml')
+        tree = ET.parse('shortcuts.xml')
+        root = tree.getroot()
+        for shortcut in root.findall('shortcut'):
+            if shortcut.find('url').text == url:
+                print("Bookmark already exists.")
+                return
+        shortcut = ET.SubElement(root, 'shortcut')
+        ET.SubElement(shortcut, 'title').text = title
+        ET.SubElement(shortcut, 'url').text = url
+        tree.write('shortcuts.xml')
 
-        # Close the database
-        conn.close()
-        # save date  using"shortcut.db"
-        c.execute("INSERT INTO shortcuts VALUES (?, ?)", (title, url))
-        conn.commit()
-        # close datebase
-        conn.close()
-
-    # read bookmark from ”shortcut.db”
     def load_shortcuts(self):
+        if not os.path.exists('shortcuts.xml'):
+            return
+        tree = ET.parse('shortcuts.xml')
+        root = tree.getroot()
+        added_urls = set()
+        for shortcut in root.findall('shortcut'):
+            title = shortcut.find('title').text
+            url = shortcut.find('url').text
+            if url not in added_urls:
+                self.add_website_shortcut(url, title)
+                added_urls.add(url)
 
-      #  connect "shortcut.db"
-      conn = sqlite3.connect('shortcuts.db')
-      c = conn.cursor()
-
-      #  get information from "shortcut.db"
-      c.execute("SELECT title, url FROM shortcuts")
-      shortcuts = c.fetchall()
-
-      #  close“shortcut.db“
-      conn.close()
-
-      # add bookmark to sidebar
-      for title, url in shortcuts:
-
-        self.add_website_shortcut(url, title)
-
-    # creating bookmarks for the specified websites
     def add_website_shortcut(self, url, name):
-        name = name[:23] + '...'if len(name) > 23 else name
+        name = name[:23] + '...' if len(name) > 23 else name
         shortcut_button = QAction(name, self)
-        shortcut_button.url = url  # URLをBookmarkActionに保存
+        shortcut_button.url = url
         view = QWebEngineView()
         view.load(QUrl(url))
         view.iconChanged.connect(lambda icon, button=shortcut_button: button.setIcon(icon))
         shortcut_button.triggered.connect(lambda: self.tabs.currentWidget().setUrl(QUrl(url)))
         self.vertical_bar.addAction(shortcut_button)
-        #  connect“shortcut.db“
-        conn = sqlite3.connect('shortcuts.db')
-        c = conn.cursor()
-        # save information to“shortcut.db“
-        c.execute("INSERT INTO shortcuts VALUES (?, ?)", (name, url))
-        conn.commit()
-        # close“shortcut.db“
-        conn.close()
+        self.save_shortcut_to_xml(name, url)
 
-    
-    def create_database():
-        #  connect“shortcut.db“
-        conn = sqlite3.connect('shortcuts.db')
-
-        # get cursor
-        c = conn.cursor()
-
-        #  make table
-        c.execute('''
-            CREATE TABLE IF NOT EXISTS shortcuts
-            (title TEXT, url TEXT)
-        ''')
-
-        # commit chenging
-        conn.commit()
-
-        #  close＂shortcut.db＂
-        conn.close()
-
-    create_database()
+    def create_database(self):
+        if not os.path.exists('shortcuts.xml'):
+            root = ET.Element("shortcuts")
+            tree = ET.ElementTree(root)
+            tree.write('shortcuts.xml')
 
 class BookmarkAction(QAction):
     def __init__(self, title, parent):
@@ -446,23 +306,18 @@ class BookmarkAction(QAction):
         contextMenu.exec_(self.mapToGlobal(point))
 
     def deleteBookmark(self):
-        conn = sqlite3.connect('shortcuts.db')
-        c = conn.cursor()
-        c.execute("DELETE FROM shortcuts WHERE url = ?", (self.url,))
-        conn.commit()
-        conn.close()
-        self.parent().removeAction(self)  # UIからアクションを削除
-    
-        
-                                                                                                                                                                                                                          
-# creating a Pyside6 application
+        tree = ET.parse('shortcuts.xml')
+        root = tree.getroot()
+        for shortcut in root.findall('shortcut'):
+            if shortcut.find('url').text == self.url:
+                root.remove(shortcut)
+                tree.write('shortcuts.xml')
+                break
+        self.parent().removeAction(self)
+
 app = QApplication(sys.argv)
-
-# setting name to the application
 app.setApplicationName("OrbBrowser")
-
-# creating MainWindow object
 window = MainWindow()
-
-# loop
+window.create_database()
+window.show()
 app.exec()
